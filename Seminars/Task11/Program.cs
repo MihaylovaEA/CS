d342590c8336aea64d6e500220eb3d83adeb6a4a
Ekaterina <k.ishchenko@yandex.ru>
2023-02-26 14:08:09 +0300

    Added Task11 DelDigit

---
+++ Seminars/Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿//11. Напишите программу, которая 
+//1. выводит случайное трёхзначное число и 
+//2. удаляет вторую цифру этого  числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine ($"Случайное трехзначное число: {number}");
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+
+int newNumber = DeleteSecond(number);
+Console.WriteLine($"Полученное число: {newNumber}");
+
+int DeleteSecond (int num)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
