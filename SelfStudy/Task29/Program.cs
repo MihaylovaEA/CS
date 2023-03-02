29863b41dbc1bf16fa77c438796095ba146a41c0
Ekaterina <k.ishchenko@yandex.ru>
2023-03-02 14:41:18 +0300

    Task 29

---
+++ SelfStudy/Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine(factorial);
+
+int Factorial (int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+        res = res * i;
+    return res;
+}
