ba7d53b8d36030109f2b610db37d9c7816536fa6
Ekaterina <k.ishchenko@yandex.ru>
2023-03-02 14:18:32 +0300

    Task 27

--- SelfStudy/Task27/Program.cs
+++ SelfStudy/Task27/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digits = Digits(number);
+Console.WriteLine(digits);
+
+int Digits (int num) 
+{
+    if (num < 0) num = -num;
+    int count = 0;
+    while (num > 0)
+    {
+        num =num / 10;
+        count++;
+    };
+    return count;
+};
