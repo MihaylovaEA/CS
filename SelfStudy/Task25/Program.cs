b3c296475768d242f97e1b63523751941fc271bc
Ekaterina <k.ishchenko@yandex.ru>
2023-03-02 13:06:12 +0300

    Task 25

---
+++ SelfStudy/Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = Sum(number);
+Console.WriteLine(sum);
+
+int Sum (int num)
+{
+    int res = 0;
+    for (int i = 1; i <= num; i++)
+        res = res + i;
+    return res;
+}
