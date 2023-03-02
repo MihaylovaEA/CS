fb5fc177fd3ae16e54751d900225e48382d5dcb1
Ekaterina <k.ishchenko@yandex.ru>
2023-03-02 14:28:16 +0300

    Task 28

--- SelfStudy/Task28/Program.cs
+++ SelfStudy/Task28/Program.cs
@@ -1,2 +1,23 @@
 ﻿// 28. Подсчитать сумму цифр в числе
 
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+Console.WriteLine(sumDigits);
+
+int SumDigits (int num) 
+{
+    if (num < 0) num = -num;
+    int count = 0;
+    int sum = 0;
+    int rem = 0;
+    while (num > 0)
+    {
+        rem = num % 10;
+        num = num / 10;
+        sum = sum + rem;
+        count++;
+    }
+    return sum;
+}
