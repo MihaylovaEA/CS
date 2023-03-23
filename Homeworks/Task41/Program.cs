997c2cf2b313cbe0beabfa96a87da5a4cf2a0e3c
Ekaterina <k.ishchenko@yandex.ru>
2023-03-23 14:38:37 +0300

    HW 6

---
+++ Homeworks/Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateUserArray (int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountNegativeElements (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число M: ");
+int sizeUserArray = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите {sizeUserArray} чисел:");
+int[] userArray = CreateUserArray(sizeUserArray);
+
+int result = CountNegativeElements(userArray);
+Console.WriteLine($"Вы ввели отрицательных чисел - {result}");
---
+++ Homeworks/Task43/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+bool Check(int b1, int k1, int b2, int k2)
+{
+    if (k1 == k2)
+    {
+        if (k1 == k2)
+        {
+            if (b1 == b2) Console.WriteLine("Прямые совпадают");
+            else Console.WriteLine("Прямые параллельны");
+        }
+        
+        return false;
+    }
+    else return true;
+}
+
+double CoordX(double b1, double k1, double b2, double k2)
+{
+    return Math.Round((b1 - b2) / (k2 - k1), 1);
+}
+
+double CoordY(double b1, double k1, double b2, double k2)
+{
+    return Math.Round((k1 * (b1 - b2) + b1 * (k2 - k1)) / (k2 - k1));
+}
+
+Console.Write("b1 = ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1 = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b2 = ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = CoordX(b1, k1, b2, k2);
+double y = CoordY(b1, k1, b2, k2);
+
+string point = Check(b1, k1, b2, k2) ? $"({x};{y})" : "";
+Console.WriteLine(point);
+
