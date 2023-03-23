c8970de8eb41ebf022556f1c6a453adb89b08fb5
Ekaterina <k.ishchenko@yandex.ru>
2023-03-23 09:54:57 +0300

    Task 34

---
+++ Homeworks/Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)//Задаем массив
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)//Выводим массив на экран
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountEven(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Задайте размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(sizeArray, 100, 999);
+int countEven = CountEven(array);
+PrintArray(array);
+Console.WriteLine(countEven);
