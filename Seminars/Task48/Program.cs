dab0f689f5cd7fb380454a1c8bbbee52a361eb3c
Ekaterina <k.ishchenko@yandex.ru>
2023-03-27 13:51:03 +0300

    Seminar 7

--- Seminars/Task48/Program.cs
+++ Seminars/Task48/Program.cs
@@ -1,50 +1,38 @@
-﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
-// которых оба индекса чётные, и замените эти элементы на их
-// квадраты.
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
+int[,] CreateMatrixByIndex(int rows, int columns)  //этот метод создает двумерный массив,
+{                                                  //в скобках указыаем первое число строки,второе столбцы
     int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
+
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = i + j;
         }
+
     }
     return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix)             //печать двумерного массива
 {
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j],3} ");
+            Console.Write($"{matrix[i, j],4} ");
         }
         Console.WriteLine();
     }
 }
 
-void SquareElementsEvenIndex(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i += 2)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j += 2)
-        {
-            matrix[i, j] *= matrix[i, j];
-        }
-    }
-}
-
-int[,] array2d = CreateMatrixRndInt(5, 5, -10, 10);
-Console.WriteLine("Матрица до преобразования:");
+int[,] array2d = CreateMatrixByIndex(3, 4);
 PrintMatrix(array2d);
-Console.WriteLine();
-Console.WriteLine("Матрица после преобразования:");
-SquareElementsEvenIndex(array2d);
-PrintMatrix(array2d);
-Console.WriteLine();
---
+++ Seminars/Task55/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] CreateNewArray(int[,] arr)
+{
+    int[,] matrix = new int[arr.GetLength(1),arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(1); i++)                      //for (int i = 0; i < matrix.GetLength(0); i++)               
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)                  ////for (int j = 0; j < matrix.GetLength(1); i++)
+        {
+            matrix[i,j] = arr[j,i];
+        }
+    }
+    return matrix;
+}
+
+
+int[,] matrix = CreateMatrixRndInt(4, 5, 0, 11);
+PrintMatrix(matrix);
+//if (matrix.GetLength(0) == matrix.GetLength(1))
+//{
+    int[,] newMatrix = CreateNewArray(matrix);
+    System.Console.WriteLine();
+    PrintMatrix(newMatrix);
+//}
+//else
+//{
+  //  Console.WriteLine("Матрица не является симметричной");
+//}
