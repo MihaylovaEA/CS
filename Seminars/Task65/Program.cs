f7e7bbb2d0bd9cf7f253319d43d3552058df332e
Ekaterina <k.ishchenko@yandex.ru>
2023-04-03 14:41:54 +0300

    Seminar 8

---
+++ Seminars/Task65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+Console.WriteLine("Введите натуральное число M");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number, number1);
+
+void NaturalNumbers(int num, int num1)
+{
+    if (num < num1)
+    {
+        NaturalNumbers(num, num1 - 1);
+        Console.Write($"{num1} ");
+    }
+
+    else if (num > num1)
+    {
+        Console.Write($"{num} ");
+        NaturalNumbers(num - 1, num1);
+    }
+
+    else
+    {
+        Console.Write($"{num} ");
+
+    }
+}
---
+++ Seminars/Task69/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      else return n * FactorialRec(n - 1);
+//  }
+
+//  Console.WriteLine(FactorialRec(10)); // 3628800
+
+
+
+
+int DegreeNumbers(int num, int num1)
+{
+    if (num1 == 0) return 1;
+    else return num * DegreeNumbers(num, num1 - 1);
+}
+
+Console.WriteLine("Введите натуральное число M");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int degreeNumbers = DegreeNumbers(number, number1);
+Console.WriteLine(degreeNumbers);
